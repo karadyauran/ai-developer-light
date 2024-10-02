7f4911044a5bd53d6a6f1047be5396e93edc4b4b
Alex Karadyaur <alex.karadyaur@outlook.com>
2024-10-02 15:14:18 +0200

    feat: Initial commit for TaskMaster application: Set up basic task management system with CRUD operations.

--- TaskMaster/handlers.go
+++ TaskMaster/handlers.go
@@ -3,63 +3,52 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
-
 	"./models"
-	"./db"
 	"./utils"
 )
 
-func TasksHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		tasks, err := db.GetTasks()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		utils.RespondWithJSON(w, http.StatusOK, tasks)
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	models.AddTask(task)
+	utils.RespondWithJSON(w, http.StatusCreated, task)
 }
 
-func TaskHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		var task models.Task
-		if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		if err := db.CreateTask(&task); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		utils.RespondWithJSON(w, http.StatusCreated, task)
-	case "PUT":
-		var task models.Task
-		if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		if err := db.UpdateTask(&task); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		utils.RespondWithJSON(w, http.StatusOK, task)
-	case "DELETE":
-		id, err := strconv.Atoi(r.URL.Query().Get("id"))
-		if err != nil {
-			http.Error(w, "Invalid task ID", http.StatusBadRequest)
-			return
-		}
-		if err := db.DeleteTask(id); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		utils.RespondWithJSON(w, http.StatusNoContent, nil)
-	default:
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	tasks := models.GetAllTasks()
+	utils.RespondWithJSON(w, http.StatusOK, tasks)
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	if err := models.UpdateTask(task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, task)
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := models.DeleteTask(task.ID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
--- TaskMaster/main.go
+++ TaskMaster/main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
 	"./handlers"
+	"./router"
 )
 
 func main() {
-	http.HandleFunc("/tasks", handlers.TasksHandler)
-	http.HandleFunc("/task", handlers.TaskHandler)
-
-	fmt.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	r := router.NewRouter()
+	
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
 }
