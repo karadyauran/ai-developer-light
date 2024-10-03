2bdafb1c1de91d0b2969ccb6c0ae98f61669a91b
Alex Karadyaur <alex.karadyaur@outlook.com>
2024-10-03 22:50:21 +0200

    Deleted all projects

--- InvoiceEase/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Invoice struct {
-	ID         string  `json:"id"`
-	ClientName string  `json:"client_name"`
-	Amount     float64 `json:"amount"`
-	Status     string  `json:"status"`
-	DueDate    string  `json:"due_date"`
-}
