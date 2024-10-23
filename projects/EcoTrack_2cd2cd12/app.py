d80866375baf18bdff01a9c4f689822671a77088
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-10-23 10:31:00 +0200

    feat: Deleted old projects

--- projects/EcoTrack_2cd2cd12/app.py
+++
@@ -1,7 +0,0 @@
-from user_interface import EcoTrackApp
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = EcoTrackApp(root)
-    root.mainloop()
--- projects/EcoTrack_be81801c/analytics.js
+++
@@ -1,9 +0,0 @@
-
-export function generateInsights() {
-    const data = fetchData();
-    const totalActivities = data.activities.length;
-    const averageImpact = totalActivities > 0 ? data.totalImpact / totalActivities : 0;
-    return {
-        totalActivities,
-        averageImpact
-    };
